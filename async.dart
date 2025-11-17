70607e0fd256c78614823b8e21cb82eb83003ff0
Alexius Osemeke <alexiusosemeke@gmail.com>
2025-11-17 16:36:51 +0100

    New update

--- async.dart
+++ async.dart
@@ -1,1 +1,12 @@
+// This talks about asynchronous prograrmming in Dart using Future, async, await, and Streams.
 
+void main() {
+  getData().then((value) => print(value));
+  print('Asynchronous programming in Dart');
+}
+
+Future getData() {
+  return Future.delayed(Duration(seconds: 5), () {
+    return 'Data fetched after 5 seconds';
+  });
+}
